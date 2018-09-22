2e6d9ef1602d93da2ef5dfaa0ce3f08013d74dcd
Jay Bowles <j.kou.bowles@gmail.com>
2018-09-22 19:06:19 +0100

    Added dummy data for My Benefits page

--- app/pages/my-benefits.js
+++ app/pages/my-benefits.js
@@ -1,20 +1,112 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import LayoutMain from '../src/layouts/main';
-
+import {
+  Row,
+  Col,
+  Card,
+  Table,
+  CardHeader,
+  CardBody,
+  Container,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 import Meta from '../src/components/Meta';
+import LayoutMain from '../src/layouts/main';
+import moment from 'moment';
+
+const data = {
+  earnings: [
+    { id: 1, name: 'Today', amount: '$12.81' },
+    { id: 2, name: 'This Month', amount: '$81.11' },
+    { id: 3, name: 'This Year', amount: '$482.81' }
+  ],
+  subscribers: [
+    {
+      name: 'NYU',
+      purpose: 'Lung cancer research',
+      rating: 71,
+      earned: 3.4,
+      startDate: 'Sat Sep 22 2018 18:11:42 GMT+0100 (British Summer Time)'
+    },
+    {
+      name: 'Johns Hopkins',
+      purpose: 'Atrial fibrillation research',
+      rating: 43,
+      earned: 4.1,
+      startDate: 'Sat Sep 10 2018 18:11:42 GMT+0100 (British Summer Time)'
+    }
+  ]
+};
 
 export default class extends Component {
-  state = {};
-
   render() {
-    const { data } = this.state;
-
     return (
       <LayoutMain>
         <Meta title="My Account" />
 
-        <Container>hello</Container>
+        <Container>
+          <Nav>
+            <NavItem>
+              <NavLink href="#earnings">Earnings</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#subscribers">Data Usage</NavLink>
+            </NavItem>
+          </Nav>
+          <section>
+            <a name="#earnings" />
+            <h4>Earnings</h4>
+            <Row>
+              {data.earnings.map(item => (
+                <Col>
+                  <Card key={item.id}>
+                    <CardBody>
+                      <h5>{item.name}</h5>
+                      <h1>{item.amount}</h1>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </section>
+          <hr />
+          <section>
+            <a name="#earnings" />
+            <h4>Data Usage</h4>
+            <Row>
+              <Col lg="8">
+                <Card>
+                  <CardHeader>Data Sold To</CardHeader>
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th>Subscriber</th>
+                        <th>Purpose</th>
+                        <th>Rating</th>
+                        <th>Earned</th>
+                        <th>Start date</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data.subscribers.map(subscriber => (
+                        <tr>
+                          <td>{subscriber.name}</td>
+                          <td>{subscriber.purpose}</td>
+                          <td>{subscriber.rating}</td>
+                          <td>{subscriber.earned}</td>
+                          <td>
+                            {moment(subscriber.startDate).format('DD MMM ‘YY')}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </Card>
+              </Col>
+            </Row>
+          </section>
+        </Container>
       </LayoutMain>
     );
   }
