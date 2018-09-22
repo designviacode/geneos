c6af195503c122db74f3969f45bcb19a000c40bb
Jay Bowles <j.kou.bowles@gmail.com>
2018-09-22 21:23:36 +0100

    Flesh out My Data page

--- app/src/components/DataPanelList.js
+++ app/src/components/DataPanelList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, Nav, NavItem, NavLink } from 'reactstrap';
+import { Row, Col, Card, CardBody, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 export default class DataPanelList extends React.PureComponent {
@@ -42,20 +42,182 @@ export default class DataPanelList extends React.PureComponent {
             </NavItem>
           ))}
         </Nav>
-        {data.data.map(item => (
-          <div key={item.id}>
-            <a name={item.slug} />
-            <Card className="mb-3">
-              <CardBody>
-                <h4>{item.name}</h4>
-                <img
-                  src="http://designplaygrounds.com/wp-content/uploads/2010/01/data-visualization-processing.jpg"
-                  responsive="true"
-                />
-              </CardBody>
-            </Card>
-          </div>
-        ))}
+        <section>
+          <h1>Genome</h1>
+          <Card>
+            <CardBody>
+              <h4>Ethnicity</h4>
+              <Row>
+                <Col>
+                  {/* <img src={require('../images/ethnicity-pie.svg')} /> */}
+                  <div>52.4%</div>
+                  <div>European</div>
+                  <div>16.4%</div>
+                  <div>East Asian & Native American</div>
+                  <div>31.2%</div>
+                  <div>Subsaharan African</div>
+                </Col>
+                <Col />
+              </Row>
+              <h4>Ancestry composition</h4>
+              <Row>
+                <Col>
+                  <h4 className="text-muted">European</h4>
+                  <div>
+                    <div>
+                      <span className="">Iberian</span>
+                      <span className="float-right">30%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Spain</span>
+                      <span className="float-right">31%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Ashkenazi</span>
+                      <span className="float-right">32%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Jewish</span>
+                      <span className="float-right">33%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Sardinian</span>
+                      <span className="float-right">34%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Southern European</span>
+                      <span className="float-right">35%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Northwestern European</span>
+                      <span className="float-right">36%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly European</span>
+                      <span className="float-right">37%</span>
+                      <div className="clearfix" />
+                    </div>
+                  </div>
+                </Col>
+                <Col>
+                  <h4 className="text-muted">East Asian & American</h4>
+                  <div>
+                    <div>
+                      <span className="">Iberian</span>
+                      <span className="float-right">30%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Spain</span>
+                      <span className="float-right">31%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Ashkenazi</span>
+                      <span className="float-right">32%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Jewish</span>
+                      <span className="float-right">33%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Sardinian</span>
+                      <span className="float-right">34%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Southern European</span>
+                      <span className="float-right">35%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Northwestern European</span>
+                      <span className="float-right">36%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly European</span>
+                      <span className="float-right">37%</span>
+                      <div className="clearfix" />
+                    </div>
+                  </div>
+                </Col>
+                <Col>
+                  <h4 className="text-muted">Subsaharan African</h4>
+                  <div>
+                    <div>
+                      <span className="">Iberian</span>
+                      <span className="float-right">30%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Spain</span>
+                      <span className="float-right">31%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Ashkenazi</span>
+                      <span className="float-right">32%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Jewish</span>
+                      <span className="float-right">33%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Sardinian</span>
+                      <span className="float-right">34%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Southern European</span>
+                      <span className="float-right">35%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly Northwestern European</span>
+                      <span className="float-right">36%</span>
+                      <div className="clearfix" />
+                    </div>
+                    <div>
+                      <span className="">Broadly European</span>
+                      <span className="float-right">37%</span>
+                      <div className="clearfix" />
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </section>
+        <section>
+          <h1>Activity</h1>
+          <Card>
+            <CardBody>
+              <Row>
+                <Col>
+                  <h4>Steps</h4>
+                </Col>
+                <Col>
+                  <h4>Distance walked</h4>
+                </Col>
+                <Col>
+                  <h4>Exercise time</h4>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </section>
       </div>
     );
   }
