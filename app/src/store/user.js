d1ff295a862b411cf750c55ab1f2a5bf4cd35b39
Jay Bowles <j.kou.bowles@gmail.com>
2018-09-23 06:24:06 +0100

    Removed default profile to fix default routing

--- app/src/store/user.js
+++ app/src/store/user.js
@@ -1,11 +1,7 @@
 import { EventEmitter } from 'events';
 
 class UserStore extends EventEmitter {
-  profile = {
-    publicKey: 'EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58',
-    privateKey: '5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7',
-    name: 'jens'
-  };
+  profile = {};
 
   setUser(profile) {
     this.profile = profile;
